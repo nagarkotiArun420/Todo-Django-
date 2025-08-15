86e1878af68268a835c57e40c107289c56815e71
Arun Nagarkoti <nagarkotiarun420@gmail.com>
2025-08-15 15:14:46 +0545

    CSS added

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     
     # TO DO
     path('todo/',include('todo.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
